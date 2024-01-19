9b975ca029a3982f4c8a328eeebf1b70a0119ea8
Yaroslav <yaro3245@gmail.com>
2024-01-19 16:27:45 +0100

    test

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const axios = require('axios')
+const app = require('express')()
+const cors = require('cors')
+
+app.use(cors())
+
+// nodemon --env-file=.env index.js
+
+const PORT = 8080
+
+let temp = process.env.TRANSISTOR_API_KEY
+
+const TRANSISTOR_API_KEY = temp.substring(1, temp.length - 1)
+
+app.get('/test', (req, res) => {
+  res.send('ABOBA')
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/episodeinfo/:podcast_id', async function (req, res) {
+  let podcast_id = req.params.podcast_id
+  res.statusCode = 200
+  let output = ''
+
+  await axios
+    .get('https://api.transistor.fm/v1/episodes/' + podcast_id, {
+      headers: {
+        'x-api-key': TRANSISTOR_API_KEY,
+      },
+    })
+    .then((transistor_response) => {
+      if (transistor_response.status == 200) {
+        output = transistor_response.data.data
+      } else {
+        output = 'ERROR'
+        res.statusCode = 404
+      }
+    })
+    .catch((err) => {
+      output = 'ERROR'
+      res.statusCode = 404
+    })
+
+  res.send(output)
+})
+
+app.listen(PORT, () => {
+  console.log('API READY')
+})
