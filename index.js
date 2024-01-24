86553ae121e0e2a535e9107ec10310608e40ffee
Yaroslav <yaro3245@gmail.com>
2024-01-24 02:38:38 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const axios = require('axios')
 const app = require('express')()
 const cors = require('cors')
+const fs = require('node:fs')
 
 app.use(cors())
 
 // nodemon --env-file=.env index.js
+
+let modelForCacheFile = {
+  last_episode: {
+    formatted: {
+      data: {},
+      last_update: '',
+    },
+    unformatted: {
+      data: {},
+      last_update: '',
+    },
+  },
+  featured_episodes: {
+    episodes_list: [],
+    data: [],
+    last_update: '',
+  },
+}
+
+let modelForFeaturedEpisodes = {
+  episodes_list: [],
+}
+
+if (fs.existsSync('cache.json')) {
+  console.log('cache exists')
+} else {
+  console.log("cache doesn't exist")
+  fs.writeFileSync('cache.json', JSON.stringify(modelForCacheFile, null, 2))
+}
+
+if (fs.existsSync('featured_episodes.json')) {
+  console.log('featured episodes list exists')
+} else {
+  console.log("featured episodes list doesn't exist")
+  fs.writeFileSync('featured_episodes.json', JSON.stringify(modelForFeaturedEpisodes, null, 2))
+}
 
 const PORT = 8080
 
@@ -84,10 +121,6 @@ function convertToFormattedTitle(title) {
 let temp = process.env.TRANSISTOR_API_KEY
 
 const TRANSISTOR_API_KEY = temp.substring(1, temp.length - 1)
-
-app.get('/test', (req, res) => {
-  res.send('ABOBA')
-})
 
 // '/episode/1525237?formatted=true'
 app.get('/episode/:episode_id', async function (req, res) {
@@ -199,6 +232,46 @@ app.get('/last-episode', async function (req, res) {
 
   let request_output = null
 
+  if (formatted) {
+    let cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'))
+    let data = cache.last_episode.formatted.data
+    let last_update = cache.last_episode.formatted.last_update
+    let last_update_date = new Date(last_update)
+    let currentDate = new Date()
+
+    let diffMs = currentDate - last_update_date
+    let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000)
+    if (
+      Object.keys(data).length !== 0 &&
+      String(last_update).length !== 0 &&
+      diffMins >= 0 &&
+      diffMins < 2
+    ) {
+      output.data = data
+      res.send(output)
+      return
+    }
+  } else {
+    let cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'))
+    let data = cache.last_episode.unformatted.data
+    let last_update = cache.last_episode.unformatted.last_update
+    let last_update_date = new Date(last_update)
+    let currentDate = new Date()
+
+    let diffMs = currentDate - last_update_date
+    let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000)
+    if (
+      Object.keys(data).length !== 0 &&
+      String(last_update).length !== 0 &&
+      diffMins >= 0 &&
+      diffMins < 2
+    ) {
+      output.data = data
+      res.send(output)
+      return
+    }
+  }
+
   await axios
     .get(
       'https://api.transistor.fm/v1/episodes?fields[episode][]=title&fields[episode][]=share_url&pagination[page]=1&pagination[per]=1',
@@ -229,7 +302,7 @@ app.get('/last-episode', async function (req, res) {
     return
   }
 
-  //reformatting the output if asked by the user
+  //reformatting the output & writing to the cache if asked
   if (formatted == true) {
     let shareurl = request_output.share_url
     let id = request_output.id
@@ -243,10 +316,134 @@ app.get('/last-episode', async function (req, res) {
       iframe: iframeurl,
     }
     request_output.id = id
+    let cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'))
+    cache.last_episode.formatted.data = request_output
+    let now = new Date()
+    cache.last_episode.formatted.last_update = now
+    fs.writeFileSync('cache.json', JSON.stringify(cache, null, 2))
+  } else {
+    let cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'))
+    cache.last_episode.unformatted.data = request_output
+    let now = new Date()
+    cache.last_episode.unformatted.last_update = now
+    fs.writeFileSync('cache.json', JSON.stringify(cache, null, 2))
   }
 
   output.data = request_output
   res.send(output)
+})
+
+app.get('/featured-episodes', function (req, res) {
+  res.statusCode = 200
+
+  let output = {
+    status: 'success',
+  }
+
+  let episodes_list = JSON.parse(fs.readFileSync('featured_episodes.json', 'utf-8'))
+  episodes_list = episodes_list.episodes_list
+  let cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'))
+  let cache_episodes_list = cache.featured_episodes.episodes_list
+
+  let data = cache.featured_episodes.data
+  let last_update = cache.featured_episodes.last_update
+  let last_update_date = new Date(last_update)
+  let currentDate = new Date()
+
+  let diffMs = currentDate - last_update_date
+  let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000)
+  //check if cache already has up-to-date information
+  if (
+    data.length !== 0 &&
+    JSON.stringify(cache_episodes_list) == JSON.stringify(episodes_list) &&
+    String(last_update).length !== 0 &&
+    diffMins >= 0 &&
+    diffMins < 6
+  ) {
+    output.data = data
+    res.send(output)
+    return
+  }
+  //check if featured episodes list has hanged since the last cache update
+  if (JSON.stringify(cache_episodes_list) != JSON.stringify(episodes_list)) {
+    cache.featured_episodes.episodes_list = episodes_list
+  }
+
+  let temp_data = []
+
+  episodes_list.map(async function (featured_episode) {
+    let request_output = null
+
+    await axios
+      .get(
+        'https://api.transistor.fm/v1/episodes/' +
+          featured_episode +
+          '?fields[episode][]=title&fields[episode][]=share_url',
+        {
+          headers: {
+            'x-api-key': TRANSISTOR_API_KEY,
+          },
+        },
+      )
+      .then((transistor_response) => {
+        if (transistor_response.status == 200) {
+          request_output = transistor_response.data.data.attributes
+        } else {
+          res.statusCode = 400
+          output.status = 'failure'
+          output.message =
+            'transistor.fm responded with invalid http code while trying to access data of episode ' +
+            featured_episode
+        }
+      })
+      .catch((err) => {
+        res.statusCode = 400
+        output.status = 'failure'
+        output.message =
+          'exception occured during axios request to data of episode ' + featured_episode
+      })
+
+    if (request_output == null) {
+      res.send(output)
+      error = true
+      return
+    }
+
+    //reformatting the output
+    let shareurl = request_output.share_url
+    let iframeurl = String(shareurl)
+    iframeurl = iframeurl.split('')
+    iframeurl[28] = 'e'
+    iframeurl = iframeurl.join('')
+    request_output = convertToFormattedTitle(request_output.title)
+    request_output.url = {
+      share: shareurl,
+      iframe: iframeurl,
+    }
+    request_output.id = featured_episode
+
+    //pushing result to the array
+    temp_data.push(request_output)
+    //checking if it was the final iteration to start sorting
+    if (temp_data.length == episodes_list.length) {
+      //reordering in the original order
+      let temp_sorted_data = []
+      for (let i = 0; i < temp_data.length; i++) {
+        let foundObject = temp_data.find((obj) => obj['id'] === episodes_list[i])
+        temp_sorted_data.push(foundObject)
+      }
+
+      //saving the output to cache and sending reply to the user
+      output.data = temp_sorted_data
+      cache.featured_episodes.data = temp_sorted_data
+      let now = new Date()
+      cache.featured_episodes.last_update = now
+      fs.writeFileSync('cache.json', JSON.stringify(cache, null, 2))
+
+      res.send(output)
+      return
+    }
+  })
 })
 
 app.listen(PORT, () => {
